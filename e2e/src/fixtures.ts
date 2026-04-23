da3d942c182dd7bbca0f3adc8829dede159097c8
Matt Raible <matt.raible@crowdstrike.com>
2026-04-23 08:46:23 -0600

    Migrate e2e tests to @crowdstrike/foundry-playwright

--- e2e/src/fixtures.ts
+++ e2e/src/fixtures.ts
@@ -1,7 +1,7 @@
 import { test as baseTest } from '@playwright/test';
-import { FoundryHomePage } from './pages/FoundryHomePage';
-import { AppManagerPage } from './pages/AppManagerPage';
-import { AppCatalogPage } from './pages/AppCatalogPage';
+import {
+  FoundryHomePage, AppManagerPage, AppCatalogPage, config,
+} from '@crowdstrike/foundry-playwright';
 import { FoundryJsDemoPage } from './pages/FoundryJsDemoPage';
 import { EventsTabPage } from './pages/EventsTabPage';
 import { CollectionsTabPage } from './pages/CollectionsTabPage';
@@ -11,7 +11,6 @@ import { WorkflowsTabPage } from './pages/WorkflowsTabPage';
 import { LogScaleTabPage } from './pages/LogScaleTabPage';
 import { ModalsTabPage } from './pages/ModalsTabPage';
 import { NavigationTabPage } from './pages/NavigationTabPage';
-import { config } from './config/TestConfig';
 
 type FoundryFixtures = {
   foundryHomePage: FoundryHomePage;
@@ -30,69 +29,19 @@ type FoundryFixtures = {
 };
 
 export const test = baseTest.extend<FoundryFixtures>({
-  page: async ({ page }, use) => {
-    const timeouts = config.getPlaywrightTimeouts();
-    page.setDefaultTimeout(timeouts.timeout);
-
-    if (!process.env.CONFIG_LOGGED) {
-      config.logSummary();
-      process.env.CONFIG_LOGGED = 'true';
-    }
-
-    await use(page);
-  },
-
-  foundryHomePage: async ({ page }, use) => {
-    await use(new FoundryHomePage(page));
-  },
-
-  appManagerPage: async ({ page }, use) => {
-    await use(new AppManagerPage(page));
-  },
-
-  appCatalogPage: async ({ page }, use) => {
-    await use(new AppCatalogPage(page));
-  },
-
-  foundryJsDemoPage: async ({ page }, use) => {
-    await use(new FoundryJsDemoPage(page));
-  },
-
-  eventsTabPage: async ({ foundryJsDemoPage }, use) => {
-    await use(new EventsTabPage(foundryJsDemoPage));
-  },
-
-  collectionsTabPage: async ({ foundryJsDemoPage }, use) => {
-    await use(new CollectionsTabPage(foundryJsDemoPage));
-  },
-
-  apiIntegrationsTabPage: async ({ foundryJsDemoPage }, use) => {
-    await use(new ApiIntegrationsTabPage(foundryJsDemoPage));
-  },
-
-  cloudFunctionsTabPage: async ({ foundryJsDemoPage }, use) => {
-    await use(new CloudFunctionsTabPage(foundryJsDemoPage));
-  },
-
-  workflowsTabPage: async ({ foundryJsDemoPage }, use) => {
-    await use(new WorkflowsTabPage(foundryJsDemoPage));
-  },
-
-  logScaleTabPage: async ({ foundryJsDemoPage }, use) => {
-    await use(new LogScaleTabPage(foundryJsDemoPage));
-  },
-
-  modalsTabPage: async ({ foundryJsDemoPage }, use) => {
-    await use(new ModalsTabPage(foundryJsDemoPage));
-  },
-
-  navigationTabPage: async ({ foundryJsDemoPage }, use) => {
-    await use(new NavigationTabPage(foundryJsDemoPage));
-  },
-
-  appName: async ({}, use) => {
-    await use(config.appName);
-  },
+  foundryHomePage: async ({ page }, use) => { await use(new FoundryHomePage(page)); },
+  appManagerPage: async ({ page }, use) => { await use(new AppManagerPage(page)); },
+  appCatalogPage: async ({ page }, use) => { await use(new AppCatalogPage(page)); },
+  foundryJsDemoPage: async ({ page }, use) => { await use(new FoundryJsDemoPage(page)); },
+  eventsTabPage: async ({ foundryJsDemoPage }, use) => { await use(new EventsTabPage(foundryJsDemoPage)); },
+  collectionsTabPage: async ({ foundryJsDemoPage }, use) => { await use(new CollectionsTabPage(foundryJsDemoPage)); },
+  apiIntegrationsTabPage: async ({ foundryJsDemoPage }, use) => { await use(new ApiIntegrationsTabPage(foundryJsDemoPage)); },
+  cloudFunctionsTabPage: async ({ foundryJsDemoPage }, use) => { await use(new CloudFunctionsTabPage(foundryJsDemoPage)); },
+  workflowsTabPage: async ({ foundryJsDemoPage }, use) => { await use(new WorkflowsTabPage(foundryJsDemoPage)); },
+  logScaleTabPage: async ({ foundryJsDemoPage }, use) => { await use(new LogScaleTabPage(foundryJsDemoPage)); },
+  modalsTabPage: async ({ foundryJsDemoPage }, use) => { await use(new ModalsTabPage(foundryJsDemoPage)); },
+  navigationTabPage: async ({ foundryJsDemoPage }, use) => { await use(new NavigationTabPage(foundryJsDemoPage)); },
+  appName: async ({}, use) => { await use(config.appName); },
 });
 
 export { expect } from '@playwright/test';
