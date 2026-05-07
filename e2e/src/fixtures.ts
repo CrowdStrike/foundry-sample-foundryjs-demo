6e4e9f98bfd37e63019ea0af0bef1368a65194d2
Matt Raible <matt.raible@crowdstrike.com>
2026-05-07 14:25:39 -0600

    Remove unused fixtures from e2e tests
    Kept all tab page fixtures and foundryJsDemoPage which are used in tests. Removed FoundryHomePage, AppManagerPage, AppCatalogPage, appName, and config fixtures that were defined but never destructured.

--- e2e/src/fixtures.ts
+++ e2e/src/fixtures.ts
@@ -1,7 +1,4 @@
 import { test as baseTest } from '@playwright/test';
-import {
-  FoundryHomePage, AppManagerPage, AppCatalogPage, config,
-} from '@crowdstrike/foundry-playwright';
 import { FoundryJsDemoPage } from './pages/FoundryJsDemoPage';
 import { EventsTabPage } from './pages/EventsTabPage';
 import { CollectionsTabPage } from './pages/CollectionsTabPage';
@@ -13,9 +10,6 @@ import { ModalsTabPage } from './pages/ModalsTabPage';
 import { NavigationTabPage } from './pages/NavigationTabPage';
 
 type FoundryFixtures = {
-  foundryHomePage: FoundryHomePage;
-  appManagerPage: AppManagerPage;
-  appCatalogPage: AppCatalogPage;
   foundryJsDemoPage: FoundryJsDemoPage;
   eventsTabPage: EventsTabPage;
   collectionsTabPage: CollectionsTabPage;
@@ -25,13 +19,9 @@ type FoundryFixtures = {
   logScaleTabPage: LogScaleTabPage;
   modalsTabPage: ModalsTabPage;
   navigationTabPage: NavigationTabPage;
-  appName: string;
 };
 
 export const test = baseTest.extend<FoundryFixtures>({
-  foundryHomePage: async ({ page }, use) => { await use(new FoundryHomePage(page)); },
-  appManagerPage: async ({ page }, use) => { await use(new AppManagerPage(page)); },
-  appCatalogPage: async ({ page }, use) => { await use(new AppCatalogPage(page)); },
   foundryJsDemoPage: async ({ page }, use) => { await use(new FoundryJsDemoPage(page)); },
   eventsTabPage: async ({ foundryJsDemoPage }, use) => { await use(new EventsTabPage(foundryJsDemoPage)); },
   collectionsTabPage: async ({ foundryJsDemoPage }, use) => { await use(new CollectionsTabPage(foundryJsDemoPage)); },
@@ -41,7 +31,6 @@ export const test = baseTest.extend<FoundryFixtures>({
   logScaleTabPage: async ({ foundryJsDemoPage }, use) => { await use(new LogScaleTabPage(foundryJsDemoPage)); },
   modalsTabPage: async ({ foundryJsDemoPage }, use) => { await use(new ModalsTabPage(foundryJsDemoPage)); },
   navigationTabPage: async ({ foundryJsDemoPage }, use) => { await use(new NavigationTabPage(foundryJsDemoPage)); },
-  appName: async ({}, use) => { await use(config.appName); },
 });
 
 export { expect } from '@playwright/test';
